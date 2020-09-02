a2c3d9e45b24f4dcd17ded7efa2bc73b47509187
Linda <ltramos7@gmail.com>
2020-09-01 20:50:35 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import './scss/main.scss'
+
+import About from './components/about'
+import Blog from './components/blog'
+import Contact from './components/contact'
+import Footer from './components/footer'
+import Home from './components/home'
+import Navigation from './components/navigation'
+import Work from './components/work'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <Navigation/>
+      <BrowserRouter>
+        <Route exact path='/about' component={About}/>
+        <Route exact path='/blog' component={Blog}/>
+        <Route exact path='/contact' component={Contact}/>
+        <Route exact path='/home' component={Home}/>
+        <Route exact path='/work' component={Work}/>
+      </BrowserRouter>
+      <Footer/>
+    </body>
   );
 }
 
